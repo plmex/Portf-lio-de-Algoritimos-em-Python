a6115b73c2a3fa1fa328aaf5a182b34e59e799fe
plmex <php.eleuterio@gmail.com>
2024-03-30 20:05:45 -0300

    Create quiz.py
    ## Quiz em Python com Biblioteca Rich
    
    Este script Python implementa um quiz interativo utilizando a biblioteca Rich para melhorar a experiência de visualização no console. O quiz lê perguntas e opções de um arquivo de texto, apresenta as perguntas de forma aleatória ao jogador e solicita respostas. Após cada resposta, o script fornece feedback instantâneo sobre a correção da resposta.
    
    ### Funcionalidades Principais:
    
    1. **Leitura de Perguntas:** O script lê perguntas e opções de resposta de um arquivo de texto formatado.
    2. **Apresentação Interativa:** As perguntas são apresentadas de forma clara e legível no console, utilizando a formatação da biblioteca Rich.
    3. **Feedback de Resposta:** Após cada resposta do jogador, o script fornece feedback imediato sobre a correção da resposta.
    4. **Preparação para Próxima Pergunta:** O script apresenta uma mensagem de preparação antes de cada nova pergunta, aumentando a tensão e o envolvimento do jogador.
    5. **Fim do Quiz:** Ao final do quiz, uma mensagem é exibida indicando o término do jogo.
    
    ### Como Utilizar:
    
    1. **Execução:** Execute o script Python em um ambiente compatível, como um terminal ou prompt de comando.
    2. **Interação:** Siga as instruções exibidas no console para inserir seu nome, indicar se deseja jogar e responder às perguntas apresentadas.
    3. **Feedback:** Receba feedback imediato sobre a correção das suas respostas e continue até o final do quiz.
    
    ### Observações:
    
    - Certifique-se de ter a biblioteca Rich instalada para uma melhor formatação no console.
    - Personalize o arquivo "questions.txt" com suas próprias perguntas e respostas para criar um quiz único e personalizado.
    
    Espero que este script seja útil para criar quizzes interativos e divertidos em Python! Se precisar de mais alguma informação ou assistência, não hesite em entrar em contato.

---
+++ quiz.py
@@ -0,0 +1,64 @@
+from rich.console import Console
+from rich.prompt import Prompt
+from random import sample
+from time import sleep
+
+#Lendo o arquivo de perguntas
+
+def reading_questions(questions):
+    perguntas = []
+    with open(questions, 'r', encoding='utf-8') as arquivo:
+       perguntas_do_arquivo = arquivo.read().split('\n\n')
+
+       for question in perguntas_do_arquivo:
+           lines = question.strip().split('\n')
+           pergunta = lines[0].split('#')[1].strip() # Coletando cada pergunta 
+           opcoes_resposta = [opcao.strip() for opcao in lines[1:-1]] # Coletando as opções de resposta
+           resposta = lines[-1].split(':')[1].strip() # Coletando a resposta correta
+           perguntas.append({"pergunta":pergunta, "opcoes_resposta":opcoes_resposta, "respostas":resposta})
+    return perguntas
+
+
+questoes = reading_questions("questions.txt")
+perguntas_aleatorias = sample(list(questoes),5)
+
+console = Console()
+
+saudacao = "Seja muito bem-vindo(a) ao quiz!"
+print('='*len(saudacao))
+print(saudacao)
+print('='*len(saudacao))
+nome = str(input('Informe o nick name: '))
+print(f'{nome}, você deseja jogar? Digite S para sim e N para não.')
+answerUser = str(input('Opção: ')).upper()[0]
+
+contador = 0
+if answerUser not in "SN":
+    while answerUser not in "SN":
+        print('Opção inválida! Tente novamente.')
+        print(f'{nome}, você deseja jogar? Digite S para sim e N para não.')
+        answerUser = str(input('Opção: ')).upper()[0]
+elif answerUser == 'N':
+    quit()
+elif answerUser == 'S':
+    for questao in perguntas_aleatorias:
+        console.print(questao["pergunta"])
+        for opcoes in questao["opcoes_resposta"]:
+            console.print(opcoes)
+        questionAnswer = Prompt.ask('Sua resposta (a,b,c,d)').lower()
+
+
+        #Verificando as respostas
+        if questionAnswer == questao["respostas"].split(')')[0].strip().lower():
+            console.print("Sua resposta está [green]correta[/green]!\n")
+        else:
+            console.print("Sua resposta está [red]incorreta[/red]!\n")
+        contador += 1
+        if contador < 4:
+            console.print('Prepare-se! A próxima pergunta vem aí!')
+            sleep(3)
+        if contador == 4:
+            console.print('Prepare-se! A última pergunta já está chegando!')
+            sleep(3)
+
+console.print("Fim do Quiz!")
